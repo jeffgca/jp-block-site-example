0478dc6ff81fce8ec0602357a5fe70a40a979b42
canuckistani <jeff@canuckistani.ca>
2014-09-06 20:59:52 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+var self = require('sdk/self');
+
+const { Ci, Cu, Cc, Cr } = require('chrome');
+Cu.import('resource://gre/modules/Services.jsm');
+
+var patternsToBlock = [new RegExp('.*\:\/\/.*\.dhnet.be\/.*','i'), new RegExp('.*\:\/\/.*\www.dhnet.be\/.*','i')];
+
+exports.main = function(options) {
+  var observers = {
+     'http-on-examine-response': {
+        observe: function(aSubject, aTopic, aData) {
+           //console.info('http-on-modify-request: aSubject = ' + aSubject + ' | aTopic = ' + aTopic + ' | aData = ' + aData);
+           var httpChannel = aSubject.QueryInterface(Ci.nsIHttpChannel);
+           var requestUrl = httpChannel.URI.spec;
+           for (var i = 0; i < patternsToBlock.length; i++) {
+              if (patternsToBlock[i].test(requestUrl)) {
+                 httpChannel.cancel(Cr.NS_BINDING_ABORTED); //or can do redirect instead of cancel like: httpChannel.redirectTo(Services.io.newURI('http://www.google.com', null, null));
+                 //console.log('requestUrl was', requestUrl, 'which mathces blocked pattern:', patternsToBlock[i], 'so blocked it');
+                 break;
+              }
+           }
+           if (requestUrl.indexOf('google.com') > -1) {
+
+           }
+        },
+        reg: function() {
+           Services.obs.addObserver(observers['http-on-modify-request'], 'http-on-modify-request', false);
+        },
+        unreg: function() {
+           Services.obs.removeObserver(observers['http-on-modify-request'], 'http-on-modify-request');
+        }
+     }
+  };
+
+  for (var o in observers) {
+    observers[o].reg();
+  }
+};
+
+exports.onUnload = function(reason) {
+  for (var o in observers) {
+    observers[o].unreg();
+  }
+};
